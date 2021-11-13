ebd8c550b8a5a86544a4f076447d885eabf767ac
vakowal <ginger.kowal@gmail.com>
2021-11-13 17:10:08 -0500

    classify properties as residential / commercial

--- property_values.R
+++ property_values.R
@@ -47,6 +47,29 @@ values_subs <- values_appraisal_years_unique[
   values_appraisal_years_unique$PIN %in% prop_val_PIN_list, ]
 values_subs$total_val <- values_subs$LandVal + values_subs$BldgVal
 
+# isolate class codes appearing in Emma
+class_codes <- read.csv("D:/PODER_Emma/Property_values/Buncombe_class_codes.csv")
+parcels_Emma <- merge(
+  parcels_df, class_codes, by.x='Class', by.y='CODE', all.x=TRUE)
+class_codes_Emma <- parcels_Emma[
+  !duplicated(parcels_Emma$Class), c('Class', 'Description', 'Ginger_class')]
+# class codes appearing in Emma, most inclusive parcel list (intersects 0.25 mile buffer)
+write.csv(class_codes_Emma, "D:/PODER_Emma/Property_values/Emma_unique_class_codes.csv")
+
+# change in quartile bins over time
+df_list = list()
+for(year in appraisal_years) {
+  subs <- values_subs[values_subs$TaxYear == year, 'total_val']
+  quant_tv_df <- data.frame(year=quantile(subs, probs=c(0.25, 0.5, 0.75, 1)))
+  colnames(quant_tv_df) <- year
+  df_list[[paste(year)]] <- quant_tv_df
+}
+quant_allyears_df <- data.frame(
+  '2005-2017'=quantile(values_subs$total_val, probs=c(0.25, 0.5, 0.75, 1)))
+df_list[['allyears']] <- quant_allyears_df
+combined_df <- do.call(cbind, df_list)
+write.csv(combined_df, "D:/PODER_Emma/Results/total_val_quantiles_by_year.csv")
+
 # calculate summary statistics by tax year
 land_val_summary <- data.frame(do.call(
   rbind, tapply(values_subs$LandVal, values_subs$TaxYear, summ_fun)))
@@ -126,24 +149,82 @@ segmented_resh <- reshape(
   drop=c('count'),
   direction='long')
 
+segmented_resh$metric <- factor(
+  segmented_resh$metric,
+  levels=c("perc_change_min", "perc_change_max", "perc_change_mean",
+           "perc_change_median", "perc_change_std"),
+  labels=c('min', 'max', 'mean', 'median', 'std'))
 p <- ggplot(segmented_resh, aes(x=TaxYear, y=value))
 p <- p + geom_point() + facet_grid(metric~segment, scales='free')
 p <- p + ylab("Percent change from 2005")
 print(p)
-pngname <- "D:/PODER_Emma/Results/Property_value_summary_by_segment.png"
-png(filename=pngname, width=6, height=6, units='in', res=300)
+pngname <- "D:/PODER_Emma/Results/Property_value_perc_change_by_segment.png"
+png(filename=pngname, width=5, height=6, units='in', res=300)
 print(p)
 dev.off()
 
-# why are maximum property values in lower 33% going up?
-# maximum parcel value goes up by a lot
-# the PIN for the property with maximum value in 2009 out of all lower-third parcels
-# is 963819239800000
-max_2009_val_pin <- 963819239800000
-test_subs <- values_df[values_df$PIN == max_2009_val_pin, ]
+# calculate percent change in value for building and land separately
+# land value only
+subs_2005 <- values_subs[values_subs$TaxYear == 2005, 'LandVal']
+landval_quantiles <- quantile(subs_2005, probs=c(0.33, 0.67, 1))
+lower_third_PIN_list <- values_subs[
+  (values_subs$TaxYear == 2005) &
+    (values_subs$LandVal < totalval_quantiles[1]), 'PIN']
+middle_third_PIN_list <- values_subs[
+  (values_subs$TaxYear == 2005) &
+    (values_subs$LandVal >= totalval_quantiles[1]) &
+    (values_subs$LandVal < totalval_quantiles[2]), 'PIN']
+upper_third_PIN_list <- values_subs[
+  (values_subs$TaxYear == 2005) &
+    (values_subs$LandVal >= totalval_quantiles[2]), 'PIN']
+lower_third_df <- values_subs[values_subs$PIN %in% lower_third_PIN_list, ]
+middle_third_df <- values_subs[values_subs$PIN %in% middle_third_PIN_list, ]
+upper_third_df <- values_subs[values_subs$PIN %in% upper_third_PIN_list, ]
 
-# why are property values in middle and upper 33% going down?
-# the minimum parcel value goes down by a lot
+lower_third_summary <- data.frame(
+  do.call(
+    rbind, tapply(lower_third_df$LandVal, lower_third_df$TaxYear, summ_fun)))
+lower_third_sum_df <- data.frame(
+  cbind(TaxYear=row.names(lower_third_summary), lower_third_summary))
+lower_third_sum_df$segment <- 'lower 33%'
+lower_third_pc_df <- relative_to_2005(lower_third_sum_df)
+middle_third_summary <- data.frame(
+  do.call(
+    rbind, tapply(middle_third_df$LandVal, middle_third_df$TaxYear, summ_fun)))
+middle_third_sum_df <- data.frame(
+  cbind(TaxYear=row.names(middle_third_summary), middle_third_summary))
+middle_third_sum_df$segment <- 'middle 33%'
+middle_third_pc_df <- relative_to_2005(middle_third_sum_df)
+upper_third_summary <- data.frame(
+  do.call(
+    rbind, tapply(upper_third_df$LandVal, upper_third_df$TaxYear, summ_fun)))
+upper_third_sum_df <- data.frame(
+  cbind(TaxYear=row.names(upper_third_summary), upper_third_summary))
+upper_third_sum_df$segment <- 'upper 33%'
+upper_third_pc_df <- relative_to_2005(upper_third_sum_df)
+
+segmented_df <- rbind(lower_third_pc_df, middle_third_pc_df, upper_third_pc_df)
+segmented_resh <- reshape(
+  segmented_df,
+  varying=c(
+    'perc_change_min', 'perc_change_max', 'perc_change_mean',
+    'perc_change_median', 'perc_change_std'),
+  v.names='value', timevar='metric', idvar=c('TaxYear', 'segment'),
+  times=c(
+    'perc_change_min', 'perc_change_max', 'perc_change_mean',
+    'perc_change_median', 'perc_change_std'),
+  drop=c('count'),
+  direction='long')
+
+segmented_resh$metric <- factor(
+  segmented_resh$metric,
+  levels=c("perc_change_min", "perc_change_max", "perc_change_mean",
+           "perc_change_median", "perc_change_std"),
+  labels=c('min', 'max', 'mean', 'median', 'std'))
+p <- ggplot(segmented_resh, aes(x=TaxYear, y=value))
+p <- p + geom_point() + facet_grid(metric~segment, scales='free')
+p <- p + ylab("Percent change from 2005 (LAND VAL)")
+print(p)
 
 # top 10 owners of property in Emma, by area
 # properties within Emma proper, no buffer (2156 parcels)
@@ -153,8 +234,7 @@ useful_cols <- c(
 parcels_Emma_df <- parcels_df[
   parcels_df$Emma_nobuf == 'Emma_within', useful_cols]
 class_codes <- read.csv("D:/PODER_Emma/Property_values/Buncombe_class_codes.csv")
-parcels_Emma <- merge(
-  parcels_Emma_df, class_codes, by.x='Class', by.y='CODE', all.x=TRUE)
+
 # TODO identify investment properties
 # commercial properties -> Ginger_class == 'commercial'
 # vacant properties -> Ginger_class == 'vacant'
